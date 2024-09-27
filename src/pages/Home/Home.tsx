7b279877c4da6a0138bc0cf9472e2d0682296329
OatsFX <30326975+oatsfx@users.noreply.github.com>
2024-09-26 22:00:12 -0700

    add(styling): logos to projects
    add(styling): links and colors to achievements
    add: more achievements
    chore(branding): logo update
    chore: fix security issue on older browsers

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import {
   BiLogoFigma,
+  BiLogoFirebase,
   BiLogoJava,
   BiLogoJavascript,
   BiLogoPython,
@@ -62,11 +63,12 @@ const Home: React.FC = () => {
             className="absolute -z-50 top-[250px] overflow-x-hidden animate-move-fade"
             fill="currentColor"
           >
-            {/* original view box 0 0 1540 747*/}
-            <path d="M1019.52,224.28c98.97,123.27,93.79,314-35.72,431.94-129.07,117.53-327.83,108.76-447.08-20.42-115.54-125.15-103.38-304.78-15.2-411.29,64.95,77.32,147.41,119.44,248.85,119.46,101.46.02,183.94-42.07,249.15-119.69Z" />
-            <path d="M504.86,688.23c-111.45,19.65-222.13,39.16-333.26,58.76-2.46-13.82-4.8-26.88-7.11-39.95-10.56-59.82-21.1-119.64-31.65-179.46-10.46-59.33-20.91-118.66-31.37-177.98-.81-4.59-.21-10.53-2.88-13.35-2.46-2.6-8.45-1.89-12.9-2.54-44.09-6.47-76.52-36.93-83.65-80.78-9.44-57.99,32.03-105.12,81.88-111.59,54.96-7.13,102.38,28.7,110.07,83.37,3.8,26.97-3.37,51.5-20.42,73.51,2.06,1.48,4.04,2.94,6.05,4.34,66.43,46.53,132.85,93.08,199.33,139.53,3.01,2.1,4.33,4.25,4.5,8.05,4.08,87.94,38.7,161.8,102.85,221.91,5.79,5.42,11.96,10.44,18.57,16.18Z" />
-            <path d="M1366.98,298.45c-24.84-35.67-28.77-72.45-7.46-110.28,19.93-35.4,61.77-54.07,100.96-46.41,41.51,8.11,72.85,40.46,78.79,81.34,8.37,57.61-34.81,109.27-93.07,111.25-2.96.1-4.31.72-4.88,3.98-13.58,77.7-27.29,155.38-40.98,233.06-10.02,56.87-20.04,113.74-30.08,170.61-.25,1.44-.66,2.85-1.16,4.97-110.97-19.57-221.64-39.08-333.09-58.73,6.4-5.54,12.28-10.38,17.87-15.53,56.88-52.47,90.69-117.07,101.32-193.72,1.3-9.38,1.81-18.9,2.23-28.38.17-3.86,1.29-6.28,4.54-8.55,67.04-46.81,134-93.75,200.97-140.66,1.35-.94,2.66-1.93,4.05-2.95Z" />
-            <path d="M770.27.03c73.19-1.44,140.37,58.71,140.67,140.05.29,77.47-62.52,140.41-140.33,140.64-77.55.22-140.58-62.51-140.63-140.3C629.92,59.03,697.14-1.25,770.27.03Z" />
+            <path d="M1017.98,225.1c82.29,94.79,105.53,275.34-11.54,406.88-118.53,133.18-322.96,142.54-453.36,20.42-131.33-122.99-125.48-314.76-31.89-427.66,65.04,77,147.4,119.09,248.75,119.03,101.04-.06,183.14-41.96,248.04-118.66Z" />
+            <path d="M910.09,140.02c.2,77.49-62.57,140.57-140.05,140.76-77.36.19-140.48-62.78-140.59-140.23C629.34,62.87,691.86.14,769.51,0c77.71-.14,140.38,62.28,140.58,140.02Z" />
+            <path d="M194.11,237.24c.14,53.6-43.28,97.23-96.87,97.36C43.74,334.74.07,291.19,0,237.61c-.07-53.73,43.17-97.12,96.88-97.22,53.75-.09,97.1,43.08,97.24,96.85Z" />
+            <path d="M1345.42,237.24c-.14,53.6,43.28,97.23,96.87,97.36,53.5.13,97.17-43.42,97.24-97,.07-53.73-43.17-97.12-96.88-97.22-53.75-.09-97.1,43.08-97.24,96.85Z" />
+            <path d="M383.28,445.74L80.47,234.52l90.37,512.52,321.08-56.62c-32.22-33.08-57.87-70.83-76.36-112.44-18.57-41.8-29.41-86.25-32.28-132.24Z" />
+            <path d="M1459.07,234.52l-302.81,211.22c-2.21,35.51-9.24,70.29-21.03,103.75-16.66,47.29-42.4,90.7-76.5,129.02-3.62,4.06-7.3,8.03-11.06,11.92l321.02,56.6,90.37-512.52Z" />
           </svg>
           <svg
             xmlns="http://www.w3.org/2000/svg"
@@ -74,10 +76,12 @@ const Home: React.FC = () => {
             className="w-28 animate-fade"
             fill="currentColor"
           >
-            <path d="M1019.52,224.28c98.97,123.27,93.79,314-35.72,431.94-129.07,117.53-327.83,108.76-447.08-20.42-115.54-125.15-103.38-304.78-15.2-411.29,64.95,77.32,147.41,119.44,248.85,119.46,101.46.02,183.94-42.07,249.15-119.69Z" />
-            <path d="M504.86,688.23c-111.45,19.65-222.13,39.16-333.26,58.76-2.46-13.82-4.8-26.88-7.11-39.95-10.56-59.82-21.1-119.64-31.65-179.46-10.46-59.33-20.91-118.66-31.37-177.98-.81-4.59-.21-10.53-2.88-13.35-2.46-2.6-8.45-1.89-12.9-2.54-44.09-6.47-76.52-36.93-83.65-80.78-9.44-57.99,32.03-105.12,81.88-111.59,54.96-7.13,102.38,28.7,110.07,83.37,3.8,26.97-3.37,51.5-20.42,73.51,2.06,1.48,4.04,2.94,6.05,4.34,66.43,46.53,132.85,93.08,199.33,139.53,3.01,2.1,4.33,4.25,4.5,8.05,4.08,87.94,38.7,161.8,102.85,221.91,5.79,5.42,11.96,10.44,18.57,16.18Z" />
-            <path d="M1366.98,298.45c-24.84-35.67-28.77-72.45-7.46-110.28,19.93-35.4,61.77-54.07,100.96-46.41,41.51,8.11,72.85,40.46,78.79,81.34,8.37,57.61-34.81,109.27-93.07,111.25-2.96.1-4.31.72-4.88,3.98-13.58,77.7-27.29,155.38-40.98,233.06-10.02,56.87-20.04,113.74-30.08,170.61-.25,1.44-.66,2.85-1.16,4.97-110.97-19.57-221.64-39.08-333.09-58.73,6.4-5.54,12.28-10.38,17.87-15.53,56.88-52.47,90.69-117.07,101.32-193.72,1.3-9.38,1.81-18.9,2.23-28.38.17-3.86,1.29-6.28,4.54-8.55,67.04-46.81,134-93.75,200.97-140.66,1.35-.94,2.66-1.93,4.05-2.95Z" />
-            <path d="M770.27.03c73.19-1.44,140.37,58.71,140.67,140.05.29,77.47-62.52,140.41-140.33,140.64-77.55.22-140.58-62.51-140.63-140.3C629.92,59.03,697.14-1.25,770.27.03Z" />
+            <path d="M1017.98,225.1c82.29,94.79,105.53,275.34-11.54,406.88-118.53,133.18-322.96,142.54-453.36,20.42-131.33-122.99-125.48-314.76-31.89-427.66,65.04,77,147.4,119.09,248.75,119.03,101.04-.06,183.14-41.96,248.04-118.66Z" />
+            <path d="M910.09,140.02c.2,77.49-62.57,140.57-140.05,140.76-77.36.19-140.48-62.78-140.59-140.23C629.34,62.87,691.86.14,769.51,0c77.71-.14,140.38,62.28,140.58,140.02Z" />
+            <path d="M194.11,237.24c.14,53.6-43.28,97.23-96.87,97.36C43.74,334.74.07,291.19,0,237.61c-.07-53.73,43.17-97.12,96.88-97.22,53.75-.09,97.1,43.08,97.24,96.85Z" />
+            <path d="M1345.42,237.24c-.14,53.6,43.28,97.23,96.87,97.36,53.5.13,97.17-43.42,97.24-97,.07-53.73-43.17-97.12-96.88-97.22-53.75-.09-97.1,43.08-97.24,96.85Z" />
+            <path d="M383.28,445.74L80.47,234.52l90.37,512.52,321.08-56.62c-32.22-33.08-57.87-70.83-76.36-112.44-18.57-41.8-29.41-86.25-32.28-132.24Z" />
+            <path d="M1459.07,234.52l-302.81,211.22c-2.21,35.51-9.24,70.29-21.03,103.75-16.66,47.29-42.4,90.7-76.5,129.02-3.62,4.06-7.3,8.03-11.06,11.92l321.02,56.6,90.37-512.52Z" />
           </svg>
 
           <p className="font-display tracking-wide text-4xl font-semibold italic animate-fade-lg pb-1">
@@ -93,6 +97,7 @@ const Home: React.FC = () => {
             <a
               href="https://github.com/OatsFX"
               target="_blank"
+              rel="noreferrer"
               className="transition hover:text-secondary"
             >
               <BsGithub size={22} />
@@ -100,6 +105,7 @@ const Home: React.FC = () => {
             <a
               href="https://youtube.com/@oatsfx"
               target="_blank"
+              rel="noreferrer"
               className="transition hover:text-secondary"
             >
               <BsYoutube size={22} />
@@ -114,6 +120,7 @@ const Home: React.FC = () => {
             <a
               href="https://behance.com/OatsFX"
               target="_blank"
+              rel="noreferrer"
               className="transition hover:text-secondary"
             >
               <BsBehance size={22} />
@@ -121,6 +128,7 @@ const Home: React.FC = () => {
             <a
               href="https://ko-fi.com/OatsFX"
               target="_blank"
+              rel="noreferrer"
               className="transition hover:text-secondary"
             >
               <SiKofi size={22} />
@@ -192,23 +200,59 @@ const Home: React.FC = () => {
             PROJECTS
           </p>
           <div className="flex flex-col lg:flex-row flex-inline gap-4 px-4 py-2">
-            <div className="flex flex-col w-full px-4 py-2 rounded-xs outline outline-1">
-              <p className="font-semibold">winnow</p>
-              <p className="text-sm">Typescript, React</p>
-              <p className="text-xs">June 2024 - Present</p>
-            </div>
-            <div className="flex flex-col w-full px-4 py-2 rounded-xs outline outline-1">
-              <p className="font-semibold">Levante</p>
-              <p className="text-sm">C#</p>
-              <p className="text-xs">August 2021 - Present</p>
-            </div>
-            <div className="flex flex-col w-full px-4 py-2 rounded-xs outline outline-1">
-              <p className="font-semibold">
-                Sivworks - Investment Web Application
-              </p>
-              <p className="text-sm">Typescript, React, Firebase</p>
-              <p className="text-xs">September 2023 - May 2024</p>
-            </div>
+            <a
+              href="https://winnow.oatsfx.com/"
+              target="_blank"
+              rel="noreferrer"
+              className="flex flex-col w-full px-4 py-2 outline outline-transparent transition-all ease-in-out border-b border-base-100 hover:outline-1 hover:outline-base-100"
+            >
+              <div className="flex justify-between">
+                <div>
+                  <p className="font-semibold">winnow</p>
+                  <p className="text-xs">June 2024 - Present</p>
+                </div>
+                <div className="flex gap-1 flex-wrap justify-end">
+                  <BiLogoTypescript size={36} />
+                  <SiReact size={36} />
+                </div>
+              </div>
+            </a>
+            <a
+              href="https://www.levante.dev/"
+              target="_blank"
+              rel="noreferrer"
+              className="flex flex-col w-full px-4 py-2 outline outline-transparent transition-all ease-in-out border-b border-base-100 hover:outline-1 hover:outline-base-100"
+            >
+              <div className="flex justify-between">
+                <div>
+                  <p className="font-semibold">Levante</p>
+                  <p className="text-xs">August 2021 - Present</p>
+                </div>
+                <div className="flex gap-1 flex-wrap justify-end">
+                  <SiCsharp size={36} />
+                </div>
+              </div>
+            </a>
+            <a
+              href="https://dev-analyzer.sivworks.com/"
+              target="_blank"
+              rel="noreferrer"
+              className="flex flex-col w-full px-4 py-2 outline outline-transparent transition-all ease-in-out border-b border-base-100 hover:outline-1 hover:outline-base-100"
+            >
+              <div className="flex justify-between">
+                <div>
+                  <p className="font-semibold">
+                    Sivworks - Investment Web Application
+                  </p>
+                  <p className="text-xs">September 2023 - May 2024</p>
+                </div>
+                <div className="flex gap-1 flex-wrap justify-end">
+                  <BiLogoTypescript size={36} />
+                  <SiReact size={36} />
+                  <BiLogoFirebase size={36} />
+                </div>
+              </div>
+            </a>
           </div>
         </div>
         <div className="flex flex-col w-2/3">
@@ -216,18 +260,46 @@ const Home: React.FC = () => {
             <FaTrophy size={24} />
             ACHIEVEMENTS
           </p>
+
           <div className="flex flex-col lg:flex-row flex-inline gap-4 px-4 py-2">
             <p>
-              I've used my platforms for good and raised over $4,500 for the
-              Bungie Foundation.
+              Recipient of Ira A. Fulton School of Engineering's{" "}
+              <span className="text-primary font-bold">Moeur Award</span> for{" "}
+              <span className="text-secondary">
+                outstanding academic achievement
+              </span>
+              .
             </p>
             <p>
-              In high school, my VeX Robotics team qualified for the Worlds
-              competition during the 2019-2020 school year.
+              Made the Ira A. Fulton School of Engineering{" "}
+              <span className="text-primary font-bold">Dean's List</span> in
+              eight consecutive semesters for{" "}
+              <span className="text-secondary">outstanding academics</span>.
             </p>
             <p>
-              My artistic creations have been featured in numerous
-              Bungie-authored blog posts.
+              In high school, my VeX Robotics team qualified for the{" "}
+              <span className="text-primary font-bold">Worlds competition</span>{" "}
+              during the 2019-2020 school year.
+            </p>
+          </div>
+          <div className="flex flex-col lg:flex-row flex-inline gap-4 px-4 py-2">
+            <p>
+              I've used my platforms for good and raised over{" "}
+              <span className="text-primary font-bold">$4,500</span> for the{" "}
+              <a
+                href="https://bungiefoundation.org/"
+                target="_blank"
+                rel="noreferrer"
+                className="text-secondary"
+              >
+                Bungie Foundation
+              </a>{" "}
+              charity.
+            </p>
+            <p>
+              My artistic creations have been{" "}
+              <span className="text-primary font-bold">featured</span> in
+              numerous Bungie-authored blog posts.
             </p>
           </div>
         </div>
